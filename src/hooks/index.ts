0468c77b6cc2c43f847b576c91a1036a0c53a9f4
Spagero763 <spagero763@users.noreply.github.com>
2026-01-29 20:22:06 +0100

    refactor: add barrel exports for hooks module

---
+++ src/hooks/index.ts
@@ -0,0 +1,4 @@
+﻿export { useContractWrite } from './useContractWrite';
+export type { TransactionState } from './useContractWrite';
+export { useRecentTransactions } from './useRecentTransactions';
+export type { RecentTransaction } from './useRecentTransactions';
